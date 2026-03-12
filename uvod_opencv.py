008e942cf70d8639dd9204dd63f0978f0725e37b
Nika Zajesek <nika.zajsek1@student.um.si>
2026-03-12 11:04:09 +0100

    Dodana logika in izracuni v telo funkcije

--- uvod_opencv.py
+++ uvod_opencv.py
@@ -5,3 +5,12 @@ BASE = Path(__file__).parent
 UTILS = BASE / ".utils"
 
 def razrezi_sliko(slika: np.ndarray, sirina_ps: int, visina_ps: int) -> list[np.ndarray]:
+    visina_slike, sirina_slike = slika.shape[:2]
+    podslike = []
+
+    for y in range(0, visina_slike, visina_ps):
+        for x in range(0, sirina_slike, sirina_ps): 
+            podslika = slika[y:y+visina_ps, x:x+sirina_ps]
+            podslike.append(podslika)
+
+    return podslike
