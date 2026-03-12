1672036b9a083a419b2e3391dec0410a339008c5
Nika Zajesek <nika.zajsek1@student.um.si>
2026-03-12 11:05:59 +0100

    Dodana logika in izracun v telo funkcije

--- uvod_opencv.py
+++ uvod_opencv.py
@@ -16,3 +16,17 @@ def razrezi_sliko(slika: np.ndarray, sirina_ps: int, visina_ps: int) -> list[np.
     return podslike
 
 def povecaj_sliko(slika: np.ndarray) -> np.ndarray: 
+    # pridobimo visino, sirino originalne slike
+    visina, sirina = slika.shape[:2]
+
+    if slika.ndim == 3: #barvna slika
+        nova_slika = np.zeros((visina * 2, sirina * 2, slika.shape[2]), dtype=slika.dtype)
+    else: # sivinska slika 
+        nova_slika = np.zeros((visina * 2, sirina * 2), dtype=slika.dtype)
+
+    nova_slika[0::2, 0::2] = slika # zgornji levi kot
+    nova_slika[1::2, 0::2] = slika # spodnji levi kot
+    nova_slika[0::2, 1::2] = slika # zgornji desni kot 
+    nova_slika[1::2, 1::2] = slika # spodnji desni kot 
+
+    return nova_slika
